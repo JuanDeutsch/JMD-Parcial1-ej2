1f876e8688df72ac9d51868c10aa68e7dfb6c774
Deutsch <juandeuto@gmail.com>
2024-08-27 23:53:57 -0500

    Sube parcial

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import RickAndMortyService from './service';
-
+import RickAndMortyService from "./service.js";
 
 // acá deberás crear una instancia del servicio RickAndMortyService
-// const service = new RickAndMortyService();
+const service = new RickAndMortyService();
 
 // esta función debe encargarse de obtener el elemento contenedor
 // y agregar los personajes obtenidos por el API, deberás llamar tu función getAllCharacters
 // iterar el arreglo de personajes y llamar a la función createCharacterCard para agregar cada personaje
 // a el contenedor puedes usar la propiedad innerHTML para esto
 
-
 function createCharacterList() {
-    // llamar primero createCharacterCard(character);
-    // llamar segundo addCharacterListeners(character);
+  // llamar primero createCharacterCard(character);
+  const personajeContainer = document.querySelector(".character-list");
+  // llamar segundo addCharacterListeners(character);
+  service
+    .getAllCharacters()
+    .then((personajes) => {
+      personajes.forEach((personaje) => {
+        const personajeCard = createCharacterCard(personaje);
+        personajeContainer.innerHTML += personajeCard;
+        addCharacterListeners(personaje);
+      });
+    })
+    .catch((error) => {
+      console.error("No se pueden obtener personajes", error);
+    });
 }
 
 // esta función debe devolver la estructura html en string de tu personaje ejemplo
@@ -24,16 +35,60 @@ function createCharacterList() {
 
 // deberás usar los elementos correctos de HTML para poder visualizar el personaje
 
+function createCharacterCard(character) {
+  const { name, status, species, origin, image } = character;
+  const statusIcon = './assets/live.svg';
+  const speciesIcon = './assets/race.svg';
+  const originIcon = './assets/planet.svg';
 
-function createCharacterCard(character) {}
+    let statusColor;
+    if (status === "Alive") {
+        statusColor = "Vivo";
+    } else if (status === "Dead") {
+        statusColor = "Muerto";
+    } else {
+        statusColor = "Desconocido";
+    }
+
+  return `
+        <div class="character">
+            <img src="${image}" alt="${name}" class="character-image">
+            <div class="character-description">
+                <div class="character-info">
+                    <h3>${name}</h3>
+                    <div class="character-detail">
+                        <img src="${statusIcon}" alt="Status Icon" id="${statusColor}" class="detail-icon">
+                        <p>${status}</p>
+                    </div>
+                    <div class="character-detail">
+                        <img src="${speciesIcon}" alt="Species Icon" class="detail-icon">
+                        <p>${species}</p>
+                    </div>
+                    <div class="character-detail">
+                        <img src="${originIcon}" alt="Origin Icon" class="detail-icon">
+                        <p>${origin}</p>
+                    </div>
+                </div>
+                <img src="./assets/heart.svg" alt="heart" class="heart-icon" id="${name}">
+            </div>
+        </div>`;
+}
 
 // esta función deberá obtener todos los personajes y deberá agregarles un evento de click en el icono de corazon
-// cuando se haga click al icono de corazon aparecer una alerta con un mensaje 
+// cuando se haga click al icono de corazon aparecer una alerta con un mensaje
 // que diga Hola soy (nombre personaje), recuerda que puedes obtener
 // el elemento target de un evento y así obtener sus propiedades
 
-function addCharacterListeners(character) {}
+function addCharacterListeners() {
+  const heartIcon = document.querySelectorAll(".heart-icon");
 
+  heartIcon.forEach((icon) => {
+    icon.addEventListener("click", (event) => {
+        const character = event.target.id; 
+        alert(`Hola soy ${character}`);
+    });
+  });
+}
 
 // por último se llama la función y se renderiza la vista
 createCharacterList();
--- src/service.js
+++ src/service.js
@@ -38,46 +38,70 @@
 //     ]
 //   }
 
-
 class RickAndMortyService {
-    // el constructor debe inicializar una variable con la url de acceso base al API
+  // el constructor debe inicializar una variable con la url de acceso base al API
 
-	constructor() {}
+  constructor() {
+    this.url = "https://rickandmortyapi.com/api/character";
+  }
 
-    
-    // este método deberá llamar al servicio y obtener los personajes
-    // deberá devolver un objeto de la siguiente manera
+  // este método deberá llamar al servicio y obtener los personajes
+  // deberá devolver un objeto de la siguiente manera
 
-    // {
-    //     "name": "Rick Sanchez",
-    //     "status": "Alive",
-    //     "species": "Human",
-    //     "firstSeen": "Earth",
-    //     "location": "Earth",
-    //     "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-    //     "student": "aqui va el nombre del estudiante",
-    //     "code": "aqui va el codigo del estudiante"
-    // }
+  // {
+  //     "name": "Rick Sanchez",
+  //     "status": "Alive",
+  //     "species": "Human",
+  //     "firstSeen": "Earth",
+  //     "location": "Earth",
+  //     "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+  //     "student": "aqui va el nombre del estudiante",
+  //     "code": "aqui va el codigo del estudiante"
+  // }
 
-    // deberá realizar el respectivo manejo de error en caso de errores al llamar el API
-    // se recomienda usar el api fetch para obtener los datos como en el siguiente ejemplo  
+  // deberá realizar el respectivo manejo de error en caso de errores al llamar el API
+  // se recomienda usar el api fetch para obtener los datos como en el siguiente ejemplo
 
-    // ejemplo con promesas
+  // ejemplo con promesas
 
-    // fetch('miurl')
-    //  .then((respuesta) => respuesta.json())
-    //  .then((mispersonajes) => {
-    //     //aqui dentro mi logica
-    //  })
+  // fetch('miurl')
+  //  .then((respuesta) => respuesta.json())
+  //  .then((mispersonajes) => {
+  //     //aqui dentro mi logica
+  //  })
 
-    //  ejemplo con async/await
+  //  ejemplo con async/await
 
-    //  const response = await fetch('miurl');
-    //  const mispersonajes = await response.json();
+  //  const response = await fetch('miurl');
+  //  const mispersonajes = await response.json();
 
-	getAllCharacters() {
-        // aqui va tu llamado al API usando fetch puedes usar promesas o asycn/await
-	}
+  async getAllCharacters() {
+    // aqui va tu llamado al API usando fetch puedes usar promesas o asycn/await
+
+    try {
+      const respuesta = await fetch(this.url);
+      if (!respuesta.ok) {
+        throw new Error("No se pueden obtener los datos");
+      }
+
+      const datos = await respuesta.json();
+
+      const personajes = datos.results.slice(0,8).map((personaje) => ({
+        image: personaje.image,
+        name: personaje.name,
+        status: personaje.status,
+        species: personaje.species,
+        origin: personaje.origin.name,
+        student: "Juan Manuel Deutsch",
+        code: "242530",
+      }));
+
+      return personajes;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  }
 }
 
 export default RickAndMortyService;
